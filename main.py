8af920b790f11d1d47598a2ae94ca52e3cffe000
Azhor Ahai <debapriya.das.blr@gmail.com>
2018-05-06 07:56:00 +0530

    added CSV writing functionality

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import ipwhois
+import datetime
 from warnings import filterwarnings
 filterwarnings(action="ignore")
-
+import csv, os
 
 def getwhoisdict(IPAddr):
     '''
@@ -12,5 +13,25 @@ def getwhoisdict(IPAddr):
     return res
 
 
+def writetocsv(mydictlist, headers):
+    ts = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    csvdir = "output"
+    csvfile = "output_ipaddr" + str(ts) + ".csv"
+    csvpath = os.path.join(csvdir, csvfile)
 
-print getwhoisdict('1.210.22.66')
+    with open(csvpath, "wb") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=headers)
+        writer.writeheader()
+        for mydict in mydictlist:
+            print mydict
+            writer.writerow(mydict)
+
+
+# print getwhoisdict('1.210.22.66')
+mynewdictlist = [{'a':1, 'b':"mystring"}, {'a':7, 'b':"mynewstring"}, {'a':7667, 'b':"mystring12344"}]
+headers = ['a', 'b']
+print writetocsv(mynewdictlist, headers)
+
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(getwhoisdict('1.1.1.1'))
